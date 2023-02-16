fbb8a025cf5e21b7a679cd32945b4deb39abe29b
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-15 20:31:45 -0500

    Initial Commit

---
+++ DemoSwipeListElement20230215/DemoSwipeListElement20230215App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwipeListElement20230215App.swift
+//  DemoSwipeListElement20230215
+//
+//  Created by Xavier on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwipeListElement20230215App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
